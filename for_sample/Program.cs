f38eeed1807bde55e6fb228cf04643b723e9910c
teymouri86 <fatemeh.teymoori1020@gmail.com>
2026-02-06 18:01:28 +0330

    upload for_sample project'

---
+++ for_sample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int g = 1; g <= i; g++)
+                {
+                    Console.Write('*');
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
